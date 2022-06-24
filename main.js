5307c2740d6d03219c9883d3f6310bb5d5b408ef
Aditya Paul <76274584+adityapaul-1234@users.noreply.github.com>
2022-06-24 20:16:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 width = screen.width;
-height = screen.height;
+height_1 = screen.height;
+height = height_1 - 100;
 html_code = "<iframe id = 'iframe_1' src='https://scratch.mit.edu/projects/708779222/embed' allowtransparency='true' width='" + width + "px' height='" + height + "px' frameborder='0' scrolling='no' allowfullscreen></iframe>"
 document.getElementById("div").innerHTML = html_code;
