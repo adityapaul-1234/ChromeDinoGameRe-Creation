e5a75e3bef96a880827f6d1c55d224c8cc309e1e
Aditya Paul <76274584+adityapaul-1234@users.noreply.github.com>
2022-06-24 20:13:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+width = screen.width;
+height = screen.height;
+html_code = "<iframe id = 'iframe_1' src='https://scratch.mit.edu/projects/708779222/embed' allowtransparency='true' width='" + width + "px' height='" + height + "px' frameborder='0' scrolling='no' allowfullscreen></iframe>"
+document.getElementById("div").innerHTML = html_code;
